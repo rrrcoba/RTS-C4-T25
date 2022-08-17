2082538ac676bd90f09a15ab1dec07277ac7e64c
Roger <108723939+rrrcoba@users.noreply.github.com>
2022-08-17 09:29:56 +0200

    Delete T25-4 directory

--- T25-4/src/main/java/com/example/demo/dao/IRoomDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.Room;
-
-public interface IRoomDAO extends JpaRepository<Room, Long>{
-
-}
