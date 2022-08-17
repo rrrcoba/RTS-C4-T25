9b43ceb18b873c8e8635b6ceefca0fd9db462617
Roger <108723939+rrrcoba@users.noreply.github.com>
2022-08-17 13:09:32 +0200

    Delete T25-1 directory

--- T25-1/src/main/java/com/example/demo/dao/IMakerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.Maker;
-
-public interface IMakerDAO extends JpaRepository<Maker, Long>{
-
-}
